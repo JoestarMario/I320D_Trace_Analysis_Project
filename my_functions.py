8427349252c4dbcdd2adc3f6789647f39e737815
Alexander Carrara <alexjcarrara@utexas.edu>
2026-04-13 12:00:38 -0500

    Opted to use pre-existing datetime module to represent time

--- my_functions.py
+++ my_functions.py
@@ -1,11 +1,12 @@
 import pandas as pd
-from event_datetime import Datetime, Date, Time
+from datetime import datetime
 
 def get_repo_dfs(dfs: dict[str, pd.DataFrame], repo_name: str) -> dict[str, pd.DataFrame]:
     return_dict: dict[str, pd.DataFrame] = {}
 
     for key, df in dfs.items():
         new_df: pd.DataFrame = df[(df['repo_name'] == repo_name)]
+        new_df['event_datetime'] = pd.to_datetime(new_df['event_datetime'], utc=True)
         return_dict[key] = new_df
 
 
