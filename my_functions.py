63f8172fa0787d8a62ecb90b0ccd7fcfb880f2a6
Alexander Carrara <alexjcarrara@utexas.edu>
2026-04-14 11:00:27 -0500

    Created function to graph pr success vs. hour

--- my_functions.py
+++ my_functions.py
@@ -1,5 +1,6 @@
 import pandas as pd
-from datetime import datetime
+from datetime import datetime, time, date
+import matplotlib.pyplot as plt
 
 def get_repo_dfs(dfs: dict[str, pd.DataFrame], repo_name: str) -> dict[str, pd.DataFrame]:
     return_dict: dict[str, pd.DataFrame] = {}
@@ -11,3 +12,31 @@ def get_repo_dfs(dfs: dict[str, pd.DataFrame], repo_name: str) -> dict[str, pd.D
 
 
     return return_dict
+
+def plot_pr_success_by_hour(pr_df: pd.DataFrame) -> None:
+    entries_data: dict[time, list[int]] = {}
+
+    for entry in pr_df.itertuples():
+        event_hour = entry.event_datetime.time().replace(minute=0, second=0, microsecond=0)
+
+        if event_hour not in entries_data:
+            entries_data[event_hour] = [0, 0]
+        if entry.pr_merge_status == "merged":
+            entries_data[event_hour][0] += 1
+        else:
+            entries_data[event_hour][1] += 1
+
+    x_axis: list[str] = []
+    y_axis: list[float] = []
+
+    for key in sorted(entries_data.keys(), key=lambda x: x.hour):
+        x_axis.append(key.strftime("%I %p"))
+        y_axis.append(entries_data[key][0] / sum(entries_data[key]))
+        
+
+    plt.bar(x_axis, y_axis)
+    plt.xticks(rotation=60)
+    plt.xlabel("Hour of day")
+    plt.ylabel("PR succesful merge rate")
+    plt.yticks(ticks=[0.0, 0.5, 1.0], labels=["0%", "50%", "100%"])
+    plt.show()
